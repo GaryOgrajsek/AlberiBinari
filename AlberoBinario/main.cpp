ec032d229287f86395f2ed23a97da830a1aefe4c
GaryOgrajsek <43636781+GaryOgrajsek@users.noreply.github.com>
2018-12-13 12:53:29 +0100

    Update main.cpp

--- AlberoBinario/main.cpp
+++ AlberoBinario/main.cpp
@@ -1,5 +1,18 @@
 #include"Albero.h"
+#include"Albero.cpp"
 
 int main() {
+	Albero<int> Natale;
+	int x;
+	for (int i = 0; i < 5; i++) {
+		cout << "Numerino: ";
+		cin >> x;
+		Natale.InserisciRicorsivo(x);
+	}
+	Natale.Preorder();
+	cout << Natale.ContaNodi();
+	cout << endl;
+	cout << Natale.ContaNodiFoglia();
+	system("PAUSE");
 	return 0;
 }
