126158aa298a983a67f440f60fa827f9469e226c
GaryOgrajsek <43636781+GaryOgrajsek@users.noreply.github.com>
2018-12-13 12:51:58 +0100

    Update Albero.h

--- AlberoBinario/Albero.h
+++ AlberoBinario/Albero.h
@@ -1,19 +1,28 @@
 #pragma once
 #include "NodoAlbero.h"
+#include "NodoAlbero.cpp"
 #include<iostream>
 template <class tipo>
 class Albero{
 private:
 	NodoAlbero<tipo>* T;
-	void InserimentoRicorsivo(tipo, NodoAlbero<tipo>*);
-
-public:
-	Albero();
+	NodoAlbero<tipo>* InserisciRicorsivo(tipo, NodoAlbero<tipo>*);
 	void Preorder(NodoAlbero<tipo>*);
 	void Inorder(NodoAlbero<tipo>*);
 	void Postorder(NodoAlbero<tipo>*);
-	void InserimentoIterativo(tipo);
-	void InserimentoRicorsivoPublic(tipo);
+	int ContaNodi(int&, NodoAlbero<tipo>*);
+	int ContaNodiFoglia(int&, NodoAlbero<tipo>*);
+	//sovrapposizione =
+
+public:
+	Albero();
+	void Preorder();
+	void Inorder();
+	void Postorder();
+	void InserisciIterativo(tipo);
+	void InserisciRicorsivo(tipo);
+	int ContaNodi();
+	int ContaNodiFoglia();
 	~Albero();
 };
 
